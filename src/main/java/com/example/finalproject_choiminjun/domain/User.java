494e7067e8076e7faae883fa27c683a3e9c52606
chlalswns200 <chlalswns200@naver.com>
2022-12-22 11:43:58 +0900

    [refacto] Audit 적용 1. db에 audit 적용 2. request에서 toEntity() 만들던 것을 entity 클래스 안에서 of()로 하기로 수정

--- src/main/java/com/example/finalproject_choiminjun/domain/User.java
+++ src/main/java/com/example/finalproject_choiminjun/domain/User.java
@@ -1,26 +1,32 @@
 package com.example.finalproject_choiminjun.domain;
 
+import com.example.finalproject_choiminjun.domain.dto.UserJoinRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.time.LocalDateTime;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
-    @Id @GeneratedValue
+public class User extends BaseEntity{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String password;
-    private LocalDateTime registeredAt;
-    private LocalDateTime updatedAt;
+    @Enumerated(EnumType.STRING)
     private UserRole role;
+    public static User of(UserJoinRequest userJoinRequest,String password) {
+        User user = User.builder()
+                .userName(userJoinRequest.getUserName())
+                .password(password)
+                .role(UserRole.NORMAL)
+                .build();
+        return user;
+    }
 }
--- src/main/java/com/example/finalproject_choiminjun/domain/dto/UserJoinRequest.java
+++ src/main/java/com/example/finalproject_choiminjun/domain/dto/UserJoinRequest.java
@@ -1,30 +1,15 @@
 package com.example.finalproject_choiminjun.domain.dto;
-import com.example.finalproject_choiminjun.domain.User;
-import com.example.finalproject_choiminjun.domain.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 @Builder
 @NoArgsConstructor
 public class UserJoinRequest {
-
     private String userName;
     private String password;
 
-    public User toEntity(String encodePassword) {
-        User user = User.builder()
-                .userName(this.userName)
-                .password(encodePassword)
-                .registeredAt(LocalDateTime.now())
-                .role(UserRole.NORMAL)
-                .build();
-        return user;
-    }
 }
