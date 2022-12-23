0ba9721aa29ef605c6b28f7515d7e74687a6e537
chlalswns200 <chlalswns200@naver.com>
2022-12-23 09:25:53 +0900

    [refacto] /api/v1/hello 수정 popin으로

--- src/main/java/com/example/finalproject_choiminjun/controller/HelloController.java
+++ src/main/java/com/example/finalproject_choiminjun/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.finalproject_choiminjun.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HelloController {
 
+    @ApiOperation(value = "cicd테스트")
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("popin");
