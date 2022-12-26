dc9c80d9f7567a5b4790d7018b3e5c7f7ac68444
chlalswns200 <chlalswns200@naver.com>
2022-12-26 15:58:18 +0900

    [refacto] 에러코드 메세지 수정 Not founded -> user를 찾을 수 없습니다.

--- src/main/java/com/example/finalproject_choiminjun/exception/ErrorCode.java
+++ src/main/java/com/example/finalproject_choiminjun/exception/ErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "UserName이 중복됩니다."),
-    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"Not founded"),
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"user를 찾을 수 없습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 잘못되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다."),
