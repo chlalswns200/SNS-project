b988500c8cc5e08a62c0a61715744722e95816be
chlalswns200 <chlalswns200@naver.com>
2022-12-29 09:59:56 +0900

    [feat] /api/v1/hello/{num} 숫자를 더해서 반환

--- src/main/java/com/example/finalproject_choiminjun/controller/HelloApiController.java
+++ src/main/java/com/example/finalproject_choiminjun/controller/HelloApiController.java
@@ -3,6 +3,7 @@ package com.example.finalproject_choiminjun.controller;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,19 @@ public class HelloApiController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok().body("최민준");
+    }
+
+    @GetMapping("/hello/{num}")
+    public ResponseEntity<Integer> sumOfDigit(@PathVariable Integer num) {
+
+        int sum = 0;
+        while (num >0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return ResponseEntity.ok().body(sum);
+
     }
 
 }
