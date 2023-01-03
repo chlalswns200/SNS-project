e6498576835078abffb3e6f3e61630306622449d
chlalswns200 <chlalswns200@naver.com>
2023-01-03 16:21:21 +0900

    [feat] 댓글 수정 기능 추가

---
+++ src/main/java/com/example/finalproject_choiminjun/domain/dto/CommentModifyResponse.java
@@ -0,0 +1,26 @@
+package com.example.finalproject_choiminjun.domain.dto;
+
+import com.example.finalproject_choiminjun.domain.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Builder
+public class CommentModifyResponse {
+
+    private Long id;
+    private String comment;
+    private String userName;
+    private Long postId;
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastModifiedAd;
+
+}
