5cf9613cacc1c34247aab59ddf6f53ad765db515
chlalswns200 <chlalswns200@naver.com>
2023-01-03 13:13:21 +0900

    [fix] swagger 수정

--- src/main/java/com/example/finalproject_choiminjun/configuration/SwaggerConfiguration.java
+++ src/main/java/com/example/finalproject_choiminjun/configuration/SwaggerConfiguration.java
@@ -19,13 +19,12 @@ import java.util.List;
 @Configuration
 public class SwaggerConfiguration {
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.spring.snsproject"))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
@@ -35,22 +34,17 @@ public class SwaggerConfiguration {
                 .securityReferences(defaultAuth())
                 .build();
     }
+
     private List<SecurityReference> defaultAuth() {
         AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
         AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
         authorizationScopes[0] = authorizationScope;
         return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
     }
+
     private ApiKey apiKey() {
         return new ApiKey("Authorization", "Authorization", "header");
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("SNS 구현 프로젝트")
-                .description("회원가입, 로그인, 글 작성 기능 제공")
-                .version("1.0.0")
-                .build();
-    }
 
 }
